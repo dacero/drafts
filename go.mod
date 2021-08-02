ee59e5f095bfadc01346bda9a540a60d58323cb9
da0 <da0@da0s-mbp.lan>
2021-08-02 07:58:23 +0200

    Complete version that reads a static file: drafts.md, and reders the markdown with a first iteration of the css template.

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/dacero/drafts
 go 1.16
 
 require (
-	github.com/nsf/gocode v0.0.0-20190302080247-5bee97b48836 // indirect
-	github.com/rogpeppe/godef v1.1.2 // indirect
-	golang.org/x/tools v0.1.5 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.15
+	github.com/russross/blackfriday/v2 v2.1.0
 )
--- main.go
+++ main.go
@@ -3,16 +3,38 @@ package main
 import (
 	"net/http"
 	"text/template"
+	"io/ioutil"
 	"log"
+	"strings"
+
+	"github.com/russross/blackfriday/v2"
+	"github.com/microcosm-cc/bluemonday"
 )
 
+type Draft struct {
+	Body    string
+}
+
+func (d Draft) HTMLBody() string {
+	groomedString := strings.ReplaceAll(d.Body, "\r\n", "\n")
+	unsafe := blackfriday.Run([]byte(groomedString))
+	output := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
+	return string(output)
+}
 
 func serveTemplate(w http.ResponseWriter, t *http.Request) {
-	template, err := template.ParseFiles("./template.html")
+	template, err := template.ParseFiles("./template.gohtml")
 	if err != nil {
 		log.Printf("Error when opening the template: %s", err)
 	}
-	err = template.Execute(w, nil)
+	
+	d, err := ioutil.ReadFile("./drafts/draft.md")
+	if err != nil {
+		log.Printf("Error when opening the draft: %s", err)
+	}
+	draft := Draft{Body: string(d)}
+		
+	err = template.Execute(w, draft)
 	if err != nil {
 		log.Printf("Error when executing the template: %s", err)
 	}
