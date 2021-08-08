bc831f0844814d89bbfc7a3edbeb033453127dff
da0 <da0@da0s-mbp.lan>
2021-08-08 19:16:36 +0200

    The form accepts a file name, reads it, and displays.

---
+++ drafts/test.md
@@ -0,0 +1,5 @@
+# This is a test
+
+It looks good as hell.
+
+And now I'm going to **break it!**.
--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"net/http"
-	"text/template"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"strings"
+	"text/template"
 
-	"github.com/russross/blackfriday/v2"
 	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
+
+	"github.com/gorilla/mux"
 )
 
 type Draft struct {
@@ -40,8 +43,37 @@ func serveTemplate(w http.ResponseWriter, t *http.Request) {
 	}
 }
 
+func viewDraft(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	filename := r.FormValue("filename")
+
+	template, err := template.ParseFiles("./template.gohtml")
+	if err != nil {
+		fmt.Fprintf(w, "Error when opening the template: %s", err)
+		return
+	}
+	
+	d, err := ioutil.ReadFile("./drafts/" + filename)
+	if err != nil {
+		fmt.Fprintf(w, "Error when opening the draft: %s", err)
+		return
+	}
+	draft := Draft{Body: string(d)}
+		
+	err = template.Execute(w, draft)
+	if err != nil {
+		fmt.Fprintf(w, "Error when executing the template: %s", err)
+		return
+	}
+}
+
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-	http.HandleFunc("/", serveTemplate)
-	log.Fatal(http.ListenAndServe(":80", nil))
+	r := mux.NewRouter()
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	r.HandleFunc("/", serveTemplate)
+	r.HandleFunc("/view", viewDraft)
+	log.Fatal(http.ListenAndServe(":80", r))
 }
